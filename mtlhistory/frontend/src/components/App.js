ed17baaca128a920d4e158fd258dbb31e1d2347b
alex.keenan <alex.y.keenan@gmail.com>
2019-12-02 16:24:24 -0500

    added the menu

--- mtlhistory/frontend/src/components/App.js
+++ mtlhistory/frontend/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import MapContainer from './MapContainer'
-
+import Header from './Header'
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <MapContainer />
       </div>
     )
