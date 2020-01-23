bc76d7c9903ece1958d78f0d33666b194f37817c
alex.keenan <alex.y.keenan@gmail.com>
2020-01-23 09:55:20 -0500

    just changed the name of the site to match url

--- mtlhistory/frontend/src/components/Header.js
+++ mtlhistory/frontend/src/components/Header.js
@@ -66,7 +66,7 @@ class Header extends Component {
         return (
             < nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
 
-                <Link to="/" className="navbar-brand">Montreal Through Time</Link>
+                <Link to="/" className="navbar-brand">Map From The Past</Link>
 
                 <div className="collapse navbar-collapse" id="navbarColor02">
                     <ul className="navbar-nav">
